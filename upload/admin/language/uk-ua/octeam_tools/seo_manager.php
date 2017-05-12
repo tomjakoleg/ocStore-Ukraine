cbe890408a34c09c0ead582245cc7092a159812c
tomjakoleg <tomjakoleg@ukr.net>
2017-05-13 00:01:38 +0300

    Create seo_manager.php

---
+++ upload/admin/language/uk-ua/octeam_tools/seo_manager.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']         = 'SEO Менеджер';
+
+// Button
+$_['button_clear_cache']    = 'Очистити кеш';
+
+// Text
+$_['text_confirm']          = 'Видалення незворотнє!Ви впевнені в цьому?';
+$_['text_success']          = 'Успішно: SEO ключові слова оновлені!';
+$_['text_success_clear']    = 'Успішно: SEO кеш очищено!';
+$_['text_description']      = 'SEO Менеджер';
+$_['text_octeam_toolset']   = 'OC Team Інструменти';
+
+// Column
+$_['column_filter_query']   = 'Запит';
+$_['column_filter_keyword'] = 'SEO ключові слова';
+$_['column_query']          = 'Запит';
+$_['column_keyword']        = 'SEO ключові слова';
+$_['column_action']         = 'Дія';
+
+// Error
+$_['error_permission']      = 'У Вас немає прав для редагування даного модуля!';
+$_['error_query']           = 'Запит повинен містити від 3 до 255 символів!';
+$_['error_keyword']         = 'SEO ключове слово наразі вже використовується!';
+?>
